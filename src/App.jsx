3dfeded21fa640a2f20b12536a335735b0755a2a
GuilhermeMosson <guimosson@gmail.com>
2024-03-26 09:43:09 -0300

    26/03 códigos de parabens ;)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import Card from './components/Card'
+import img01 from "../assets/Imgs/foto-1.png"
+import img02 from "../assets/Imgs/foto-2.jfif"
+import img03 from "../assets/Imgs/foto-3.jpg"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <Card
+      title = 'Foto lua'
+      image = {img01}
+      text01 = "descrição da paisagem"
+      text02 = "Curitiba/PR"
+      />
+      <Card
+      title = 'Foto do Sol'
+      image = {img02}
+      text01 = "descrição da paisagem"
+      text02 = "Curitiba/PR"
+      />
+      <Card
+      title = 'Foto Da Terra'
+      image = {img03}
+      text01 = "descrição da paisagem"
+      text02 = "Curitiba/PR"
+      />
+    </>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import img01 from "../assets/Imgs/foto-1.png"
+import img02 from "../assets/Imgs/foto-2.jfif"
+import img03 from "../assets/Imgs/foto-3.jpg"
+
+
+export default function Card(props){
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <img src={props.image} alt={props.title} width={200} height="auto"/>
+            <p>{props.text1}</p>
+            <p>{props.text2}</p>
+        </div>
+    )
+}
